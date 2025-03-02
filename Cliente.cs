6462b07e20ee4e2403a19843f71339ef7b408632
Rodrigo García <rodrigo_rgp839@icloud.com>
2025-03-01 18:36:28 -0600

    Delete Cliente.cs

--- Cliente.cs
+++
@@ -1,8 +0,0 @@
-CREATE TABLE [IM252Cliente](
-	[Id] [uniqueidentifier] NOT NULL,
-	[Nombre] [nvarchar](256) NOT NULL,
-	[Direccion] [nvarchar](512) NULL,
-	[Telefono] [nvarchar](128) NOT NULL,
-	[Correo] [nvarchar](256) NOT NULL,
-    CONSTRAINT PK_IM252Cliente PRIMARY KEY ([Id])
-);
